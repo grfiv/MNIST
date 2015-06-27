463607070628f330999dcc14ad5f4aa0b79b5a64
George Fisher <george@georgefisher.com>
2015-06-27 19:34:52 -0400

    load data into R

--- load_TrainTest.R
+++ load_TrainTest.R
@@ -32,3 +32,20 @@ testX = testX[test.shuffle,]
 testY = testY[test.shuffle]
 
 rm(train.shuffle, test.shuffle)
+
+show_digit <- function(arr784, col=gray(12:1/12), ...) {
+    image(matrix(arr784, nrow=28)[,28:1], col=col, ...)
+}
+
+print_16 = function(starting_at=1, X=trainX, Y=trainY) {
+    # print a 4x4 of images in the training set
+    # starting at index=starting_at
+    opar = par(no.readonly=TRUE)
+    par(mfrow=c(4,4))
+    for (i in seq(from=starting_at, length.out=16)){
+        show_digit(matrix(as.numeric(X[i,]),28,28),
+                   main=Y[i],
+                   xlab=paste("index",i))
+    }
+    par(opar)
+}
